69f231ca4ccffdcb2e7b56fc7d9f0a1a11a2fde0
Aki <msf5pbsi@gmail.com>
2022-01-20 10:26:32 +0900

    added c++ file

--- c++/test_casting.h
+++ c++/test_casting.h
@@ -1,7 +1,53 @@
 #ifndef TEST_CASTING
 #define TEST_CASTING
+
+#include<iostream>
+#include<cassert>
+
 struct A {};
-struct B : A{};
+struct B : A {};
+
+
+// const cast: changes the mutability of the object
+// static_cast: implicit conversion where overloading possible, downcast or upcast though it's safer to use dynamic_cast
+// dynamic_cast: used for polymorphism
+// reinterpret_cast: change interepretation of the dtype
+// type casting generates a rvalue
+void test_casting() {
+
+	// const_cast can not be used on const variable
+	// const_cast is used to remove const-ness from const function or const pointer
+	const int immutable = 777;
+	std::cout << "immutable = " << immutable << " " << &immutable << std::endl;
+	auto mut = const_cast<int*>(&immutable); // not allowed, undefined behavior
+	std::cout << "mut = " << *mut << " " << mut << std::endl;
+
+
+	A a;
+	B b;
+	std::cout <<"type a = " << typeid(a).name() << std::endl;
+
+	auto ab = static_cast<B&>(a); // downcast requires a reference to the data type pointer
+	auto ba = static_cast<A>(b); // upcast requires nothing
+	std::cout << "type ab = " << typeid(ab).name() << std::endl;
+	std::cout << "type ba = " << typeid(ba).name() << std::endl;
+
+	// downcast with dynamic_cast only awllowed with polymophic class with virtual functions to store vtable
+	// 
+	auto ba_ptr = dynamic_cast<A*>(&b); // upcast allowed
+	std::cout << "type ba_ptr = " << typeid(ba_ptr).name() << std::endl;
+	// auto bab_ptr = dynamic_cast<B*>()
+	// auto bab_ptr = dynamic_cast<B*>(&a); // downcast not allowed because no vtable
+
+	int i = 99;
+	void* v;
+	v = &i;
+	std::cout << "type v = " << typeid(v).name() << std::endl;
+	// change the interpretation of data type in the memory
+	auto vv = reinterpret_cast<int*>(v);
+	std::cout << "type vv = " << typeid(vv).name() << std::endl;
+
+}
 
 
 #endif
