b21b502748faf4f21481ef8ee74aa0aed3c9a4af
Aki <msf5pbsi@gmail.com>
2022-01-23 14:51:18 +0900

    added python code

---
+++ python/test_function_scope.py
@@ -0,0 +1,20 @@
+def test_function_scope():
+    print('x', id(x), x) # funciton looks outside the scope for the value with the same name
+    # x = 777  # error cannot modify the value outside the fucntion
+    flag = False
+    def inner():
+        nonlocal flag  # now this function can modify the value outside this function
+        flag = True
+        return flag
+    
+    inner()
+    return flag
+
+
+if __name__ == '__main__':
+    x = 77
+    print('x', id(x), x)
+    ret = test_function_scope()
+    assert(ret == True)
+    print('ret value = ', ret)
+    
