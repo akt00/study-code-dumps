ca25cef805d51f26899dbad6ec98a7d7476d8da3
Aki T <84285825+akt00@users.noreply.github.com>
2022-02-23 12:16:56 +0900

    Update test_allocator.cpp

--- c++/test_allocator.cpp
+++ c++/test_allocator.cpp
@@ -18,23 +18,25 @@ void test_allocator() {
 
 	int* ptr = new int{ 9 };
 	std::cout << "ptr = " << ptr << " &ptr = " << &ptr << std::endl;
-	delete ptr;
+	delete ptr; // destroies and deallocates the object
 	std::cout << "delete ptr = " << ptr << " &ptr = " << &ptr << std::endl;
 
 
 	std::allocator<obj> alloc;
 	std::cout << typeid(alloc).name() << std::endl;
 
-	// decltype(alloc)::value_type n = 19; // int
+	// decltype(std::allocator<int>)::value_type n = 19; // int
 
 	using trait = std::allocator_traits<decltype(alloc)>;
+	// allocates the memory
 	auto *atr = trait::allocate(alloc, 1);
+	// constructs the object
 	trait::construct(alloc, atr);
 
 	std::cout << " atr = " << atr << " &atr = " << &atr << std::endl;
-	trait::destroy(alloc, atr);
+	trait::destroy(alloc, atr); // destroies the object
 	std::cout << " atr = " << atr << " &atr = " << &atr << std::endl;
-	trait::deallocate(alloc, atr, 1);
+	trait::deallocate(alloc, atr, 1); // decallocates the memory space
 	std::cout << " atr = " << atr << " &atr = " << &atr << std::endl;
 }
 
