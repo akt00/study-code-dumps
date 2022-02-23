7adfd41f324b31dd898b4f9699e151f3c6c044ab
Aki T <84285825+akt00@users.noreply.github.com>
2022-02-23 12:14:35 +0900

    Create test_allocator.cpp

---
+++ c++/test_allocator.cpp
@@ -0,0 +1,41 @@
+#ifndef TEST_ALLOCATOR
+#define TEST_ALLOCATOR
+
+#include<iostream>
+#include<memory>
+
+class obj {
+public:
+	obj() {
+		std::cout << "obj constructed...\n";
+	}
+	~obj() {
+		std::cout << "obj destoryed...\n";
+	}
+};
+
+void test_allocator() {
+
+	int* ptr = new int{ 9 };
+	std::cout << "ptr = " << ptr << " &ptr = " << &ptr << std::endl;
+	delete ptr;
+	std::cout << "delete ptr = " << ptr << " &ptr = " << &ptr << std::endl;
+
+
+	std::allocator<obj> alloc;
+	std::cout << typeid(alloc).name() << std::endl;
+
+	// decltype(alloc)::value_type n = 19; // int
+
+	using trait = std::allocator_traits<decltype(alloc)>;
+	auto *atr = trait::allocate(alloc, 1);
+	trait::construct(alloc, atr);
+
+	std::cout << " atr = " << atr << " &atr = " << &atr << std::endl;
+	trait::destroy(alloc, atr);
+	std::cout << " atr = " << atr << " &atr = " << &atr << std::endl;
+	trait::deallocate(alloc, atr, 1);
+	std::cout << " atr = " << atr << " &atr = " << &atr << std::endl;
+}
+
+#endif
