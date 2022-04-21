22796e1c6b22476def3ea7926d1a133dc86ac6e1
Aki T <84285825+akt00@users.noreply.github.com>
2022-04-21 23:58:16 +0900

    Update test_perfect_forwarding.h

--- c++/test_perfect_forwarding.h
+++ c++/test_perfect_forwarding.h
@@ -1,1 +1,18 @@
+#ifndef TEST_PERFECT_FORWARDING
+#define TEST_PERFECT_FORWARDING
 
+
+template<typename T>
+void testVal(T&& val) {
+	std::cout << "val = " << val << std::endl;
+	std::cout << "type = " << typeid(val).name() << std::endl;
+	return;
+}
+
+test_perfect_forwarding() {
+  int val = 12;
+  testVal(val);
+  testVal(14);
+}
+
+#endif
