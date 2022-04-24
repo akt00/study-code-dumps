fce34fb69adcd8384061ec3c1c45098cdc426b09
Aki T <84285825+akt00@users.noreply.github.com>
2022-04-25 06:51:05 +0900

    Create test_forward.h

---
+++ c++/test_forward.h
@@ -0,0 +1,15 @@
+#ifndef TEST_FORWARD
+#define TEST_FORWARD
+
+// accepts only rvalue
+void test_forward_helper(int&& arg) {
+    std::cout << arg << std::endl;
+}
+
+void test_forward() {
+  int i = 99;
+  test_forward(std::forward<int>(i)); // converting lvalue to rvalue
+}
+
+
+#endif // TEST_FORWARD
