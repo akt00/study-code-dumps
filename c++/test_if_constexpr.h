609a0bdd9ce253cdb169f337b7c0761b906cd874
Aki T <84285825+akt00@users.noreply.github.com>
2022-05-06 23:59:38 +0900

    Update test_if_constexpr.h

--- c++/test_if_constexpr.h
+++ c++/test_if_constexpr.h
@@ -1,10 +1,9 @@
 #ifndef TEST_IF_CONSTEXPR
 #define TEST_IF_CONSTEXPR
-
-
 #include<vector>
 
 
+// dynamically determines the return type
 template<typename T, typename U>
 T generate_array(int size) {
 	std::srand(static_cast<unsigned int>(std::time(0)));
