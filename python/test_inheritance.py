f480b8d639f0fb5c4f9efd8e22bef27316604974
Aki <84285825+akt00@users.noreply.github.com>
2022-12-05 18:54:25 +0900

    Update test_inheritance.py

--- python/test_inheritance.py
+++ python/test_inheritance.py
@@ -8,7 +8,8 @@ class Child(Base):
         super(Child, self).__init__()
         print('child called')
 
-
+# super(instance, self) is equal to just calling super().__init__()
+# super(child, self) calls the constructor of the base class of child, which is base
 class Child2(Child):
     def __init__(self):
         super(Child2, self).__init__() # calls the constructor of super(class, self).__init__()
