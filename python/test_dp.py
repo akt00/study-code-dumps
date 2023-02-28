3b5be7f9acb3c540431f0b31c2c9e3cc26e36775
Aki <84285825+akt00@users.noreply.github.com>
2023-02-28 18:24:50 +0900

    Create test_dp.py

---
+++ python/test_dp.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def mcm(p):
+    n = len(ins) - 1
+    m = np.zeros(shape=(n+1, n+1))
+
+    for l in range(1, n+1):
+        print(l)
+        for i in range(0, n-l+1):
+            j = i + l
+            m[i, j] = np.inf
+            print(i, j)
+            # k starts at i and ends at j - 1
+            for k in range(i, j):
+                left_bound = i - 1
+                p1 = p[left_bound][1] if left_bound >= 0 else p[0][0]
+                q = m[i, k] + m[k+1, j] + p1 * p[k][1] * p[j][1]
+
+                print(f'm[{i},{k}] + m[{k+1},{j}] + p{left_bound}p{k}p{j} = {q}')
+                if q < m[i, j]:
+                    m[i, j] = q
+    return m
+
+
+if __name__ == '__main__':
+    ins = [(30, 35), (35, 15), (15, 5), (5, 10), (10, 20), (20, 25)]
+    print(mcm(ins))
