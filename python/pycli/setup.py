2402a7df509952d8a799bde23869167e418257b0
Aki <84285825+akt00@users.noreply.github.com>
2023-05-23 21:08:51 +0900

    Update setup.py

--- python/pycli/setup.py
+++ python/pycli/setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
-
+# pypi package name can contain chars such as '-'
+# library name must only consits of lower alnum chars
 setup(
   name='py-cli',
   version='0.1.0',
