62820dca5a66e19237b7626eeb09277772a5d06d
Aki <84285825+akt00@users.noreply.github.com>
2023-05-23 21:04:26 +0900

    Update setup.py

--- python/pycli/setup.py
+++ python/pycli/setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 
 setup(
-  name = 'py-cli',
-  version = '0.1.0',
+  name='py-cli',
+  version='0.1.0',
   packages=['pycli'],
   entry_points={'console_scripts': ['py-cli=pycli.main:main']},
   install_requires=['pyyaml']
