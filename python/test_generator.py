17cfc4ad95543962dd6b7c2be6a4f9ac958de3d6
Aki <84285825+akt00@users.noreply.github.com>
2023-07-27 22:01:56 +0900

    Update test_generator.py

--- python/test_generator.py
+++ python/test_generator.py
@@ -1,19 +1,47 @@
-#generator performs lazy evaluation and returns an lazy iterator
-
-def csv_reader(filepath):
-    for row in open(filepath, 'r'):
-        yield row
+coordinate = (0, 0)
 
 
-def infinite_sequence():
-    num = 0
+def move_left(steps: int):
+    for _ in range(steps):
+        global coordinate
+        yield -1, 0
+        print(f'move left: {coordinate}')
+
+
+def move_up(steps: int):
+    for _ in range(steps):
+        global coordinate
+        yield 0, 1
+        print(f'move up: {coordinate}')
+
+
+def composite():
+    global coordinate
+    yield from move_left(5)
+    yield from move_up(5)
+
+
+def run_composite():
+    global coordinate
+    for g in composite():
+        coordinate = (coordinate[0] + g[0], coordinate[1] + g[1])
+
+
+def count(ceil: int):
+    counter = 0
+    while counter < ceil:
+        res = yield counter
+        counter = int(res) if res else counter + 1
+
+
+def run_count():
+    gen = count(100)
     while True:
-        yield num
-        num += 1
-
-
-if __name__ == '__main__':
-
-    ret3 = infinite_sequence()
-    for i in ret3:  # performs lazy evaluation when called
-        print(i, end=" ")
+        try:
+            res = next(gen)
+            if res == 5:
+                print(gen.send(97))
+            else:
+                print(res)
+        except StopIteration:
+            break
